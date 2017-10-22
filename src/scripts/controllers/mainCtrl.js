26e05ba1ebeeaa8816ecd967689738812e7c4eaf
Alex <xx01alex@yahoo.com>
2017-10-22 19:54:15 +0300

    Toogle active class on menu

--- src/scripts/controllers/mainCtrl.js
+++ src/scripts/controllers/mainCtrl.js
@@ -2,10 +2,31 @@ mainCtrl.$inject = ['$scope', 'instagramService'];
 
 export default function mainCtrl($scope, instagramService) {
     $scope.$on('$routeChangeStart', function(next, current) { 
-        if(current.$$route.originalPath === '/contact') {
+        var path = current.$$route.originalPath;
+        if(path === '/contact') {
             document.getElementsByTagName('body')[0].classList.add('contact');
         } else {
             document.getElementsByTagName('body')[0].classList.remove('contact');
         }
+
+        var menuItems = document.querySelectorAll('.desktop-nav ul li a');
+
+        function removeClasses(elements) {
+            elements.forEach(function(elem) {
+                elem.classList.remove('active');
+            })
+        }
+        
+        if(path === '/') {
+            removeClasses(menuItems);
+            menuItems[0].classList.add('active');
+        } else if(path === '/portfolio') {
+            removeClasses(menuItems);
+            menuItems[1].classList.add('active');
+        } else if(path === '/contact'){
+            removeClasses(menuItems);
+            menuItems[2].classList.add('active');
+        }
+
       });
 }
